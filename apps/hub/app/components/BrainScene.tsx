67a7c6834e94f2cfaabd986f9903a27379180476
HyunJemmI <dlguswp010731@gmail.com>
2026-03-24 20:03:18 +0900

    Set brain menu spline scene

--- apps/hub/app/components/BrainScene.tsx
+++ apps/hub/app/components/BrainScene.tsx
@@ -1,7 +1,7 @@
 import Spline from "@splinetool/react-spline/next";
 import styles from "../page.module.css";
 
-const BRAIN_SCENE = "loading...";
+const BRAIN_SCENE = "https://prod.spline.design/EXPOU2fNqMpVU5e5/scene.splinecode";
 
 export function BrainScene() {
   if (!BRAIN_SCENE.startsWith("http")) {
